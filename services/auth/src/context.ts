9d9c6f4465a494f18d589a1cf038cf9240e16859
vs222qj <vs222qj@student.lnu.se>
2023-01-10 01:27:12 +0100

    updated context

--- services/auth/src/context.ts
+++ services/auth/src/context.ts
@@ -1,3 +1,9 @@
-export interface Context {
+import express from 'express'
+import { AuthTokenHelper } from './util/authTokenHelper'
+
+export interface IContext {
   token?: string
+  req: express.Request
+  res: express.Response
+  ath: AuthTokenHelper
 }
