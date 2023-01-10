b0154f603917c8e213bf1ac8e9060908624bfbc1
Denni Wernersson <dw222fy@student.lnu.se>
2023-01-10 14:24:21 +0100

    Implement register & login routes

--- services/user-write/src/controllers/user-controller.ts
+++ services/user-write/src/controllers/user-controller.ts
@@ -1,11 +1,14 @@
 import createError from 'http-errors'
+import bcrypt from 'bcrypt'
+import validator from 'validator'
 import { Request, Response, NextFunction } from 'express'
 import { prisma } from '../config/prisma'
+const { isEmail } = validator
 
 export class UserController {
-  async loadUser (req: Request, res: Response, next: NextFunction, id: number) {
+  async loadUser (req: Request, res: Response, next: NextFunction, id: string) {
     try {
-      const user = await prisma.user.findFirst({ where: { id }})
+      const user = await prisma.user.findFirst({ where: { id: Number(id) }})
 
       if (!user) {
         next(createError(404))
@@ -15,6 +18,58 @@ export class UserController {
       req.user = user
 
       next()
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async register (req: Request, res: Response, next: NextFunction) {
+    try {
+      if (!isEmail(req.body.email)) {
+        next(createError(404, `${req.body.email} is not a valid email address.`))
+        return
+      }
+
+      const user = await prisma.user.create({
+        data: {
+          email: req.body.email,
+          username: req.body.username,
+          password: await bcrypt.hash(req.body.password, 10),
+        }
+      })
+
+      res
+      .status(201)
+      .json(user.id)
+    } catch (error: any) {
+      let err = error
+      if (err.code === 'P2002') {
+        let message = 'Duplicated keys' // Placeholder message
+ 
+        if (err.message.includes('email_UNIQUE')) message = 'A user is already registered to that email address.'
+        if (err.message.includes('username_UNIQUE')) message = 'That username is already taken.'
+        // Duplicated keys.
+        err = createError(409, message)
+      }
+
+      next(err)
+    }
+  }
+
+  async login (req: Request, res: Response, next: NextFunction) {
+    try {
+      const user = await prisma.user.findFirst({ where: { email: req.body.email }})
+      
+      if (!user || !(await bcrypt.compare(req.body.password, user.password))) {
+        next(createError(401, 'Invalid email or password.'))
+        return
+      }
+
+      // TODO: What should happen here?
+
+      res
+        .status(200)
+        .json('you did it yay') //
     } catch (error) {
       next(error)
     }
--- services/user-write/src/routes/user-router.ts
+++ services/user-write/src/routes/user-router.ts
@@ -8,8 +8,17 @@ const controller = new UserController()
 
 const baseURL = process.env.BASE_URL
 
-// Redirect to API
-router.get('/', (req, res, next) => res.redirect(baseURL + '/'))
-
 // Provide req.user to the route if :id exists in the route path.
 router.param('id', (req, res, next, id) => controller.loadUser(req, res, next, id))
+
+// Find a user (Sanity check - TODO: remove)
+router.get('/:id', (req, res, next) => controller.findOne(req, res, next))
+
+// Find all users (Sanity check - TODO: remove)
+router.get('/', (req, res, next) => controller.findAll(req, res, next))
+
+// Register
+router.post('/register', (req, res, next) => controller.register(req, res, next))
+
+// Login
+router.post('/login', (req, res, next) => controller.login(req, res, next))
