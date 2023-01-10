994d5253480486ec3af8ca53956c9c815ccd51ff
Denni Wernersson <dw222fy@student.lnu.se>
2023-01-10 19:52:29 +0100

    Fix isOwner & correct authJWT middleware sequences

--- services/user-write/src/routes/profile-router.ts
+++ services/user-write/src/routes/profile-router.ts
@@ -7,41 +7,39 @@ import { authenticateJWT } from './router'
 export const router = express.Router()
 const controller = new ProfileController()
 
-const baseURL = process.env.BASE_URL
-
 /**
  * Request authorization middleware, verifies ownership of the requested profile.
  */
 export const isOwner = (req: Request, res: Response, next: NextFunction) => {
-  req.profile.user_id === req.user.id ? next() : next(createError(403))
+  req.profile.user_id === req.account.id ? next() : next(createError(403))
 }
 
 // Provide req.profile to the route if :id exists in the route path.
 router.param('id', (req, res, next, id) => controller.loadProfile(req, res, next, id))
 
 // Add a new profile
-router.post('/',
-  authenticateJWT,
+router.post('/', 
+  (req, res, next) => authenticateJWT(req, res, next),
   (req, res, next) => controller.create(req, res, next)
 )
 
 // Update an entire profile
 router.put('/:id',
-  authenticateJWT,
+  (req, res, next) => authenticateJWT(req, res, next),
   (req, res, next) => isOwner(req, res, next),
   (req, res, next) => controller.replace(req, res, next)
 )
 
 // Partially update a profile
 router.patch('/:id',
-  authenticateJWT,
+  (req, res, next) => authenticateJWT(req, res, next),
   (req, res, next) => isOwner(req, res, next),
   (req, res, next) => controller.modify(req, res, next)
 )
 
 // Delete a profile
 router.delete('/:id',
-  authenticateJWT,
+  (req, res, next) => authenticateJWT(req, res, next),
   (req, res, next) => isOwner(req, res, next),
   (req, res, next) => controller.delete(req, res, next)
 )
