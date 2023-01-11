10b5846dc7be61ffdf751902ff6680081755fcfb
Denni Wernersson <dw222fy@student.lnu.se>
2023-01-11 21:29:26 +0100

    Clean up router

--- services/user-write/src/routes/profile-router.ts
+++ services/user-write/src/routes/profile-router.ts
@@ -7,30 +7,26 @@ import { authenticateJWT } from './router'
 export const router = express.Router()
 const controller = new ProfileController()
 
-// Add a new profile
+// Add a new profile: remove this?
 router.post('/', 
   (req, res, next) => authenticateJWT(req, res, next),
-  (req, res, next) => controller.loadProfile(req, res, next),
   (req, res, next) => controller.create(req, res, next)
 )
 
 // Replace the authenticated user's profile
 router.put('/',
   (req, res, next) => authenticateJWT(req, res, next),
-  (req, res, next) => controller.loadProfile(req, res, next),
   (req, res, next) => controller.replace(req, res, next)
 )
 
 // Partially update the authenticated user's profile
 router.patch('/',
   (req, res, next) => authenticateJWT(req, res, next),
-  (req, res, next) => controller.loadProfile(req, res, next),
   (req, res, next) => controller.modify(req, res, next)
 )
 
-// Delete the authenticated user's profile
+// Delete the authenticated user's profile: remove this?
 router.delete('/',
   (req, res, next) => authenticateJWT(req, res, next),
-  (req, res, next) => controller.loadProfile(req, res, next),
   (req, res, next) => controller.delete(req, res, next)
 )
