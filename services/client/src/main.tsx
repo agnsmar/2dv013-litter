982d1bad37af791884362f458108e900f8f90d72
vs222qj <vs222qj@student.lnu.se>
2023-01-12 20:41:38 +0100

    add apollo client

--- services/client/src/main.tsx
+++ services/client/src/main.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { AppRouter } from './components/router'
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
 import './index.css'
+
+const client = new ApolloClient({
+  uri: 'http://api-gateway:8888/graphql', // Change to env?
+  cache: new InMemoryCache(),
+  credentials: 'include'
+})
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <AppRouter />
+    <ApolloProvider client={client}>
+      <AppRouter />
+    </ApolloProvider>
   </React.StrictMode>
 )
