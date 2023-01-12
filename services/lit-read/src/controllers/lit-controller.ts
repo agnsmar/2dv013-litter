369a440a84ba1c280ee119cebca668dc71c0423c
Denni Wernersson <dw222fy@student.lnu.se>
2023-01-12 16:12:04 +0100

    Add lit findAll fetching

--- services/lit-read/src/controllers/lit-controller.ts
+++ services/lit-read/src/controllers/lit-controller.ts
@@ -1,14 +1,21 @@
 import createError from 'http-errors'
 import { Request, Response, NextFunction } from 'express'
 import { prisma } from '../config/prisma'
+import { Prisma } from '@prisma/client'
 
 export class LitController {
   async findAll(req: Request, res: Response, next: NextFunction) {
     try {
       const user_id = Number(req.params.id)
-      const result = await prisma.lit.findMany({ where: { user_id } })
+      
+      if(isNaN(user_id)) {
+        next(createError(400, 'ID must be a number'))
+        return
+      }
 
-      res.json(user_id)
+      const lits = await prisma.lit.findMany({ where: { user_id } })
+
+      res.json(lits)
     } catch (error) {
       next(error)
     }
