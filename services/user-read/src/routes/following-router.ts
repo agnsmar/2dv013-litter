53159f5c4e7cb312b05970683c209427c23a8f9a
Denni Wernersson <dw222fy@student.lnu.se>
2023-01-12 15:21:00 +0100

    Use AuthJWT middleware when fetching all followings

--- services/user-read/src/routes/following-router.ts
+++ services/user-read/src/routes/following-router.ts
@@ -2,9 +2,13 @@ import createError from 'http-errors'
 import express from 'express'
 import { Request, Response, NextFunction } from 'express'
 import { ProfileController } from '../controllers/profile-controller'
+import { authenticateJWT } from './router'
 
 export const router = express.Router()
 const controller = new ProfileController()
 
 // Find all followings of the authenticated user
-router.get('/', (req, res, next) => controller.findAll(req, res, next))
+router.get('/',
+  (req, res, next) => authenticateJWT(req, res, next),
+  (req, res, next) => controller.findAll(req, res, next)
+)
