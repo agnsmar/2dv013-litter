da8fd4422fd324ef4e3fd09b673b3b131d4975a3
Denni Wernersson <dw222fy@student.lnu.se>
2023-01-12 19:10:59 +0100

    Add functionality for fetching several users lits

--- services/lit-read/src/routes/lit-router.ts
+++ services/lit-read/src/routes/lit-router.ts
@@ -6,5 +6,11 @@ import { LitController } from '../controllers/lit-controller'
 export const router = express.Router()
 const controller = new LitController()
 
+// Find all lits belonging to a given collection of users
+router.get('/', (req, res, next) => controller.findAll(req, res, next))
+
+// Provide req.profile to the route if :id exists in the route path.
+router.param('id', (req, res, next, id) => controller.prepQuery(req, res, next, id))
+
 // Find all lits belonging to the given ID
 router.get('/:id', (req, res, next) => controller.findAll(req, res, next))
