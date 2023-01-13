eb4ddee2ea744cc59877bcfc62bf2d91790c1aa4
Denni Wernersson <dw222fy@student.lnu.se>
2023-01-13 15:12:31 +0100

    Add create & delete methods

--- services/user-write/src/controllers/following-controller.ts
+++ services/user-write/src/controllers/following-controller.ts
@@ -3,9 +3,49 @@ import { Request, Response, NextFunction } from 'express'
 import { prisma } from '../config/prisma'
 
 export class FollowingController {
+  async prepQuery (req: Request, res: Response, next: NextFunction, id: string) {
+    try {
+      req.body.id = Number(id)
+
+      if (isNaN(req.body.id)) {
+        next(createError(400, 'ID must be a number.'))
+        return
+      }
+
+      next()
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  // This could be minimized using upsert()
   async create (req: Request, res: Response, next: NextFunction) {
     try {
-      res.json('Hello World')
+      const followee_id: number = req.body.id
+
+      if (followee_id === req.account.id) {
+        next(createError(401, 'A User cannot follow themselves'))
+        return
+      }
+
+      const followee = await prisma.user.findUnique({ where: { id: followee_id }})
+      const follower = await prisma.user.findUnique({ where: { id: req.account.id }})
+
+      if (!followee || !follower) {
+        next(createError(404, 'User or follower does not exist'))
+        return
+      }
+
+      const following = await prisma.following.create({ 
+        data: {
+          followee_id: followee_id,
+          follower_id: req.account.id
+        }
+      })
+
+      res
+        .status(201)
+        .json(following)
     } catch (error) {
       next(error)
     }
@@ -13,7 +53,17 @@ export class FollowingController {
 
   async delete (req: Request, res: Response, next: NextFunction) {
     try {
-      res.json('Hello World')
+      const followee_id: number = req.body.id
+      await prisma.following.deleteMany({
+        where: {
+          followee_id: followee_id,
+          follower_id: req.account.id
+        } 
+      })
+
+      res
+        .status(204)
+        .send()
     } catch (error) {
       next(error)
     }
