372fc565bb6423f97b32a42b46681ee64a836e3b
Denni Wernersson <dw222fy@student.lnu.se>
2023-01-13 15:12:11 +0100

    Add pre-query middleware

--- services/user-write/src/routes/following-router.ts
+++ services/user-write/src/routes/following-router.ts
@@ -7,14 +7,17 @@ import { authenticateJWT } from './router'
 export const router = express.Router()
 const controller = new FollowingController()
 
+// Provide user id to the request if :id exists in the route path
+router.param('id', (req, res, next, id) => controller.prepQuery(req, res, next, id))
+
 // Add a new follwing relation between a given user and the authenticated user
-router.post('/', 
+router.post('/:id', 
   (req, res, next) => authenticateJWT(req, res, next),
   (req, res, next) => controller.create(req, res, next)
 )
 
 // Remove a new follwing relation between a given user and the authenticated user
-router.delete('/',
+router.delete('/:id',
   (req, res, next) => authenticateJWT(req, res, next),
   (req, res, next) => controller.delete(req, res, next)
 )
