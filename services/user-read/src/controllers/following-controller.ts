1d46bd1a7bfe2da2a7fdb189fd898bbd7be06896
Denni Wernersson <dw222fy@student.lnu.se>
2023-01-13 15:52:21 +0100

    Prettify following collection

--- services/user-read/src/controllers/following-controller.ts
+++ services/user-read/src/controllers/following-controller.ts
@@ -5,9 +5,15 @@ import { prisma } from '../config/prisma'
 export class FollowingController {
   async findAll (req: Request, res: Response, next: NextFunction) {
     try {
-      const followings = await prisma.following.findMany({where: { follower_id: req.account.id }})
+      const followings = await prisma.following.findMany({ where: { follower_id: req.account.id }})
 
-      res.json(followings)
+      const followee_ids: number[] = []
+
+      followings.forEach(following => {
+        followee_ids.push(following.followee_id)
+      })
+
+      res.json(followee_ids)
     } catch (error) {
       next(error)
     }
