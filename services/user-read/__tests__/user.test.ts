ba4fe9885655dcff53977f9270cb58d6c8d087aa
Denni Wernersson <dw222fy@student.lnu.se>
2023-01-23 16:15:18 +0100

    Rewrite tests to use beforeAll

--- services/user-read/__tests__/user.test.ts
+++ services/user-read/__tests__/user.test.ts
@@ -2,13 +2,16 @@ import request from 'supertest'
 const baseURL = 'http://localhost/api/usersread'
 
 describe('GET /api/users', () => {
+  let response: any
+  beforeAll(async () => {
+    response = await request(baseURL).get('/users')
+  })
+
   it('Should return a 200', async () => {
-    const response = await request(baseURL).get('/users')
     expect(response.statusCode).toBe(200)
   })
 
   it('Should return an array', async () => {
-    const response = await request(baseURL).get('/users')
     expect(Array.isArray(response.body)).toBe(true)
   })
 })
