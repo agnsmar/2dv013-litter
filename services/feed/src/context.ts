b367e990085d3fe1bfb9692aabf99e82549ba9de
vs222qj <vs222qj@student.lnu.se>
2023-01-25 21:15:02 +0100

    update context

--- services/feed/src/context.ts
+++ services/feed/src/context.ts
@@ -1,6 +1,10 @@
 import express from 'express'
+import amqlib from 'amqplib'
+import type { IJwtCustomPayload } from './types/types'
 
 export interface IContext {
   req: express.Request
   res: express.Response
+  conn: amqlib.Connection
+  token?: IJwtCustomPayload | null
 }
