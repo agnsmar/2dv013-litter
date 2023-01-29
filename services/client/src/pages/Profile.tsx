27d023f372cf725472835611e68ce7365e56664e
Johanna Wirell <jw223gx@student.lnu.se>
2023-01-29 10:36:30 +0100

    Add profile css

--- services/client/src/pages/Profile.tsx
+++ services/client/src/pages/Profile.tsx
@@ -5,7 +5,18 @@ export const Profile = () => {
     <>
     <Navigation />
     <div className="profile-container">
-      <div className="info-container">Info</div>
+      <div className="info-container">
+        <div className="image">
+          <img src="../../public/litter.png" alt="profile"></img>
+        </div>
+        <div className="text-container">
+          <p>Username</p>
+          <p>Description</p>
+        </div>
+        <div className="followers-container">
+          Followers
+        </div>
+      </div>
       <div className="lits-container">Lits</div>
     </div>
     </>
