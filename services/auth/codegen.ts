1bccc54dfc3a5ab02fc28b132aa4a059b59ba437
vs222qj <vs222qj@student.lnu.se>
2023-02-08 00:17:01 +0100

    update codegen config

--- services/auth/codegen.ts
+++ services/auth/codegen.ts
@@ -1,11 +1,9 @@
-import type { CodegenConfig } from '@graphql-codegen/cli'
-
 const config = {
   overwrite: true,
   schema: 'src/graphql/schema.graphql',
   generates: {
     'src/generated/graphql.ts': {
-      plugins: ['typescript', 'typescript-resolvers'],
+      plugins: ['typescript', 'typescript-resolvers', 'typescript-operations'],
       config: {
         contextType: '../context#IContext'
       }
