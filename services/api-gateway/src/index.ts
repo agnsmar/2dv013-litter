e8597cb949ad2f7942b31a72f0e1bb50dea4cfd5
Denni Wernersson <dw222fy@student.lnu.se>
2023-02-13 16:23:37 +0100

    Try cors origin

--- services/api-gateway/src/index.ts
+++ services/api-gateway/src/index.ts
@@ -82,7 +82,7 @@ const main = async () => {
 
   app.use(
     '/graphql',
-    cors({ credentials: true }),
+    cors({ origin: true, credentials: true }),
     bodyParser.json(),
     cookieParser(),
     expressMiddleware(server, { context: async ({ req, res }) => ({ req, res }) })
