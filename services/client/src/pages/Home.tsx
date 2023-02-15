94e07a2e0c5372fe5b193b2bfca1030a01ba12fa
Johanna Wirell <jw223gx@student.lnu.se>
2023-02-15 17:55:33 +0100

    Add fetch lits

--- services/client/src/pages/Home.tsx
+++ services/client/src/pages/Home.tsx
@@ -1,44 +1,47 @@
 import { Navigation } from './Navigation'
 import { Lit } from '../components/Lit'
-
-const litter = '../../public/litter.png'
-
-const getFollowing = (onlineId: string) => {
-  // TODO: hämta following lits 
-  return [{
-  username: 'testuser',
-  id: '1',
-  profile_img: litter,
-  lits: [{
-    id: 1,
-    text: 'test1'
-  }, {
-    id: 2,
-    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic quas culpa officia earum veniam maiores quisquam, delectus totam. Nobis reprehenderit, quia placeat quidem vero maxime ipsa blanditiis ullam architecto atque!'
-  }],
-  followers: ['1', '2', '3'],
-  following: ['1', '2', '3'],
-  description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic quas culpa officia earum veniam maiores quisquam, delectus totam. Nobis reprehenderit, quia placeat quidem vero maxime ipsa blanditiis ullam architecto atque!'
-  }]
-}
+import { useFeedLazyQuery } from '../generated/graphql'
+import { useState } from 'react'
+import InfiniteScroll from 'react-infinite-scroll-component'
 
 export const Home = () => {
-  const onlineId = '1'
-  const isOnline = true // TODO: kolla om är online
-  const following = getFollowing(onlineId)
+  const [hasMore, setHasMore] = useState(true)
+  const [offSet, setOffSet] = useState(0)
+  const [feed, setFeed] = useState<any[]>([])
+  const [getFeed] = useFeedLazyQuery()
+
+  const fetchMore = async () => {
+    const take = 20
+    const { data } = await getFeed({ variables: { offset: offSet, take }})
+    if (data?.feed) {
+      setFeed([...feed, ...data?.feed])
+    }
+    setOffSet(offSet + take)
+  }
+
   return (
     <div className="home-container">
-      <Navigation isOnline={isOnline} id={onlineId}/>
+      <Navigation />
       <div className="lits-container">
-        {following.map(user => user.lits.map(lit => 
-          <Lit 
-            isLiked={false}
-            image={user.profile_img}
-            username={user.username}
-            text={lit.text}
-            key={lit.id}
-          />
-        ))}
+        <InfiniteScroll 
+          dataLength={feed.length}
+          hasMore={hasMore}
+          next={fetchMore}
+          loader={<div className="loading">Loading...</div>}
+          scrollableTarget='lits-container'
+          endMessage={<div className="empty">No more lits to show</div>}
+        >
+          {feed.map((lit, i) => 
+            <Lit 
+            image={/* TODO: add image*/ ''}
+            username={lit.username}
+            text={lit.content}
+            createdAt={lit.createdAt}
+            userid={lit.user_id}
+            key={i}
+            />
+          )}
+        </InfiniteScroll>
         </div>
     </div>
   )
