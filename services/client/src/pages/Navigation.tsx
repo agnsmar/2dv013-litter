4eb255c50aca4821ce92b17d5da34e7e8bfc7a6a
vs222qj <vs222qj@student.lnu.se>
2023-02-16 01:39:31 +0100

    fix logout not working bug

--- services/client/src/pages/Navigation.tsx
+++ services/client/src/pages/Navigation.tsx
@@ -1,28 +1,59 @@
+import { useNavigate } from 'react-router'
 import { useLogoutMutation, useMeQuery } from '../generated/graphql'
 
 export const Navigation = () => {
-  const { data: onlineUser, loading: isMeLoading } = useMeQuery({ fetchPolicy: 'no-cache'})
+  const navigate = useNavigate()
+  const { data: onlineUser, loading: isMeLoading } = useMeQuery({ fetchPolicy: 'no-cache' })
   const [logout] = useLogoutMutation()
-  
-  return ( 
-    <div className="nav-container">
-      <div className="header">
-        <a href="/"><img
-          src='/litter.png'
-          alt='litter'
-          className='logo'
-        /> </a>
+
+  const handleLogout = async () => {
+    await logout()
+    navigate('/login')
+  }
+
+  return (
+    <div className='nav-container'>
+      <div className='header'>
+        <a href='/'>
+          <img
+            src='/litter.png'
+            alt='litter'
+            className='logo'
+          />{' '}
+        </a>
       </div>
-      <div className="link-container">
-        <a className="link-item" href="/">Home</a>
-        {isMeLoading ? <div className="loading">Loading...</div> : 
-        onlineUser ? (
+      <div className='link-container'>
+        <a
+          className='link-item'
+          href='/'
+        >
+          Home
+        </a>
+        {isMeLoading ? (
+          <div className='loading'>Loading...</div>
+        ) : onlineUser?.me ? (
           <>
-            <a className="link-item" href={`/profile/${onlineUser.me?.id}`}>Profile</a>
-            <a className="link-item" onClick={() => logout()} href="/login">Logout</a>
+            <a
+              className='link-item'
+              href={`/profile/${onlineUser.me?.id}`}
+            >
+              Profile
+            </a>
+            <a
+              className='link-item'
+              onClick={handleLogout}
+            >
+              Logout
+            </a>
           </>
-        ) : <a className="link-item" href="/login">Login</a>
-        }
+        ) : (
+          <a
+            className='link-item'
+            href='/login'
+          >
+            Login
+          </a>
+        )}
       </div>
     </div>
   )
