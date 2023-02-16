17ce50577f1433128407e56d14b179465b7ac1a7
vs222qj <vs222qj@student.lnu.se>
2023-02-16 01:37:12 +0100

    add error 404 to router

--- services/client/src/components/router.tsx
+++ services/client/src/components/router.tsx
@@ -4,16 +4,37 @@ import { Home } from '../pages/Home'
 import { Login } from '../pages/Login'
 import { Register } from '../pages/Register'
 import { Profile } from '../pages/Profile'
+import { Error404 } from '../pages/Error404'
 
 export const AppRouter = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/error-500" element={<Error500 />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/profile/:id" element={<Profile />} />
+        <Route
+          path='/'
+          element={<Home />}
+        />
+        <Route
+          path='/error-404'
+          element={<Error404 />}
+        />
+
+        <Route
+          path='/error-500'
+          element={<Error500 />}
+        />
+        <Route
+          path='/login'
+          element={<Login />}
+        />
+        <Route
+          path='/register'
+          element={<Register />}
+        />
+        <Route
+          path='/profile/:id'
+          element={<Profile />}
+        />
       </Routes>
     </Router>
   )
