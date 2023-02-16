9c5bf01f7266f4e76c0406e50580e157d9b67bcd
vs222qj <vs222qj@student.lnu.se>
2023-02-16 01:39:09 +0100

    add create lit input and fixed serveral bugs

--- services/client/src/pages/Home.tsx
+++ services/client/src/pages/Home.tsx
@@ -1,48 +1,121 @@
 import { Navigation } from './Navigation'
 import { Lit } from '../components/Lit'
-import { useFeedLazyQuery } from '../generated/graphql'
-import { useState } from 'react'
+import {
+  FeedQueryResult,
+  ProfileDocument,
+  useAddLitMutation,
+  useFeedLazyQuery,
+  useFeedQuery,
+  useMeQuery
+} from '../generated/graphql'
+import { useEffect, useState } from 'react'
 import InfiniteScroll from 'react-infinite-scroll-component'
 
+type TFeed = NonNullable<NonNullable<FeedQueryResult['data']>['feed']>
+
 export const Home = () => {
+  const { data: meData, loading: isMeLoading } = useMeQuery({ fetchPolicy: 'no-cache' })
   const [hasMore, setHasMore] = useState(true)
-  const [offSet, setOffSet] = useState(0)
-  const [feed, setFeed] = useState<any[]>([])
+  const [offset, setOffset] = useState(0)
+  const [feed, setFeed] = useState<TFeed>([])
+  const [addLit] = useAddLitMutation()
+  const [isCreatingLit, setIsCreatingLit] = useState(false)
+  const { data: feedData, loading: isFeedLoading } = useFeedQuery({
+    variables: { offset, take: 20 }
+  })
   const [getFeed] = useFeedLazyQuery()
+  const [litContent, setLitContent] = useState('')
 
   const fetchMore = async () => {
     const take = 20
-    const { data } = await getFeed({ variables: { offset: offSet, take }})
-    if (data?.feed) {
-      setFeed([...feed, ...data?.feed])
+    setOffset(offset + take)
+    const { data } = await getFeed({ variables: { offset, take } })
+    if (data && data.feed) {
+      if (data?.feed?.length < take) {
+        setHasMore(false)
+      }
+      setFeed([...feed, ...data.feed])
     }
-    setOffSet(offSet + take)
+  }
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    setIsCreatingLit(true)
+    await addLit({ variables: { content: litContent } })
+    setIsCreatingLit(false)
+  }
+
+  const handleLitChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const maxLitLength = 14
+    if (litContent.length >= maxLitLength) {
+      return
+    }
+    setLitContent(e.currentTarget.value)
+  }
+
+  useEffect(() => {
+    if (feedData && feedData.feed) {
+      setFeed([...feedData.feed])
+    }
+  }, [isFeedLoading, feedData])
+
+  if (isMeLoading || isFeedLoading) {
+    return <div className='loading'>Loading...</div>
   }
 
   return (
-    <div className="home-container">
+    <div className='home-container'>
       <Navigation />
-      <div className="lits-container">
-        <InfiniteScroll 
-          dataLength={feed.length}
-          hasMore={hasMore}
-          next={fetchMore}
-          loader={<div className="loading">Loading...</div>}
-          scrollableTarget='lits-container'
-          endMessage={<div className="empty">No more lits to show</div>}
-        >
-          {feed.map((lit, i) => 
-            <Lit 
-            image={/* TODO: add image*/ ''}
-            username={lit.username}
-            text={lit.content}
-            createdAt={lit.createdAt}
-            userid={lit.user_id}
-            key={i}
-            />
+      {meData?.me && (
+        <form onSubmit={handleSubmit}>
+          <label htmlFor='lit-input'>Make a lit</label>
+          <input
+            id='lit-input'
+            name='lit-input'
+            value={litContent}
+            onChange={handleLitChange}
+          />
+          {isCreatingLit ? (
+            <div className='loading'>Loading...</div>
+          ) : (
+            <button type='submit'>Lit</button>
           )}
-        </InfiniteScroll>
+        </form>
+      )}
+      {meData?.me ? (
+        <div className='lits-container'>
+          <InfiniteScroll
+            dataLength={feed.length}
+            hasMore={hasMore}
+            next={fetchMore}
+            loader={<div className='loading'>Loading...</div>}
+            scrollableTarget='lits-container'
+            endMessage={<div className='empty'>No more lits to show</div>}
+          >
+            {feed.map((lit, i) => {
+              return lit ? (
+                <Lit
+                  image={/* TODO: add image*/ ''}
+                  username={lit.username}
+                  text={lit.content}
+                  createdAt={lit.created_at}
+                  userid={lit.user_id.toString()}
+                  key={i}
+                />
+              ) : (
+                <></>
+              )
+            })}
+          </InfiniteScroll>
         </div>
+      ) : (
+        <div>
+          <span>
+            {' '}
+            To view your feed please <a href='/login'>login</a>
+          </span>{' '}
+        </div>
+      )}
     </div>
   )
 }
