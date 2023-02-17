c33c7d80dbb5e3ea1ce8107e91df23d33b2ab801
vs222qj <vs222qj@student.lnu.se>
2023-02-17 02:09:56 +0100

    add infinite scroll

--- services/client/src/pages/Profile.tsx
+++ services/client/src/pages/Profile.tsx
@@ -4,17 +4,23 @@ import { Lit } from '../components/Lit'
 import { Loading } from '../components/Loading'
 import { useNavigate, useParams } from 'react-router-dom'
 import {
+  LitsQueryResult,
   useFollowMutation,
   useIsFollowingQuery,
+  useLitsLazyQuery,
   useMeQuery,
   useProfileQuery,
   useUnfollowMutation
 } from '../generated/graphql'
+import InfiniteScroll from 'react-infinite-scroll-component'
+
+type TLits = NonNullable<NonNullable<NonNullable<LitsQueryResult['data']>['lits']>['data']>
 
 export const Profile = () => {
   const navigate = useNavigate()
   const { data: onlineUser, loading: isMeLoading } = useMeQuery({ fetchPolicy: 'no-cache' })
   const [isLoading, setIsLoading] = useState(false)
+  const [isFetched, setIsFetched] = useState(false)
   let { id } = useParams()
 
   if (!id) {
@@ -25,16 +31,39 @@ export const Profile = () => {
     loading: isProfileLoading,
     error
   } = useProfileQuery({ variables: { userid: id }, fetchPolicy: 'no-cache' })
+  const [getLits] = useLitsLazyQuery()
   const [follow] = useFollowMutation()
   const [unfollow] = useUnfollowMutation()
   const { data: isFollowingData, loading: isFollowingLoading } = useIsFollowingQuery({
     variables: { userid: id }
   })
+  const [lits, setLits] = useState<TLits>([])
+  const [hasMore, setHasMore] = useState(true)
+  const [offset, setOffset] = useState(0)
   const [isFollowing, setIsFollowing] = useState(false)
+
+  const fetchMore = async () => {
+    const take = 20
+    const { data } = await getLits({
+      variables: { offset, take, userid: id ?? '' },
+      fetchPolicy: 'no-cache'
+    })
+    if (data && data.lits.data) {
+      if (data.lits.data.length < take) {
+        setHasMore(false)
+      }
+      setLits([...lits, ...data.lits.data])
+    }
+    setOffset(offset + take)
+  }
 
   useEffect(() => {
     isFollowingData && setIsFollowing(isFollowingData.isFollowing)
-  }, [isFollowingData])
+    if (!isFetched) {
+      fetchMore()
+      setIsFetched(true)
+    }
+  }, [isFollowingData, isFetched])
 
   const handleFollow = async () => {
     setIsLoading(true)
@@ -71,12 +100,12 @@ export const Profile = () => {
         <div className='info-container'>
           <div className='profile-image'>
             <img
-              src={profileData?.profile?.profile?.avatar}
+              src={profileData?.profile?.data?.avatar}
               alt='profile'
             />
           </div>
           <div className='profile-info-container'>
-            <p className='username'>{profileData?.profile?.profile?.username}</p>
+            <p className='username'>{profileData?.profile?.data?.username}</p>
             {onlineUser?.me?.id === parseInt(id) || !onlineUser?.me ? (
               <></>
             ) : (
@@ -86,38 +115,40 @@ export const Profile = () => {
                 ) : (
                   <button
                     className='follow-button'
-                    onClick={() => handleFollow()}
+                    onClick={handleFollow}
                   >
                     {isFollowing ? 'following' : 'follow'}
                   </button>
                 )}
               </div>
             )}
-            <div className='profile-info'>
-              <div className='lits-nmr'>
-                <p>
-                  {profileData?.profile?.profile?.lits
-                    ? profileData.profile?.profile?.lits.length
-                    : 0}{' '}
-                  lits
-                </p>
-              </div>
-            </div>
-            <div className='text-container'>{profileData?.profile?.profile?.content}</div>
+            <div className='profile-info'></div>
+            <div className='text-container'>{profileData?.profile?.data?.content}</div>
           </div>
         </div>
-        <div className='lits-container'>
-          {profileData?.profile?.profile?.lits &&
-            profileData?.profile?.profile?.lits.map((lit, i) => (
+        <div
+          className='lits-container'
+          id='scrollableDiv'
+        >
+          <InfiniteScroll
+            dataLength={lits.length}
+            hasMore={hasMore}
+            next={fetchMore}
+            loader={<Loading />}
+            scrollableTarget='scrollableDiv'
+            endMessage={<div className='empty'>No more lits to show</div>}
+          >
+            {lits.map((lit, i) => (
               <Lit
-                image={profileData?.profile?.profile?.avatar || ''}
-                username={profileData?.profile?.profile?.username || ''}
+                image={profileData?.profile.data?.avatar || ''}
+                username={profileData?.profile?.data?.username || ''}
                 text={lit?.content || ''}
                 createdAt={lit?.created_at || ''}
                 userid={id || ''}
                 key={i}
               />
             ))}
+          </InfiniteScroll>
         </div>
       </div>
     </div>
